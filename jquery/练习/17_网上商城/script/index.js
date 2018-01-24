a5e3d9b30e198a12ba2b0916b140b06fc1ce5791
tengyue1997 <632013215@qq.com>
2018-01-24 14:31:39 +0800

    es6

--- jquery/练习/17_网上商城/script/index.js
+++ jquery/练习/17_网上商城/script/index.js
@@ -1,3 +1,21 @@
-/**
- * Created by apple on 17/12/3.
- */
+//最新动态
+$('.tooltip').on('mouseover',function (e) {
+    this.currTitle= this.title;
+    $('<div id="tip">'+this.currTitle+'</div>').appendTo('body').offset({
+        left:e.pageX+20,
+        top:e.pageY+20
+    });
+    $(this).attr('title','');
+}).on('mousemove',function (e) {
+    $('#tip').offset({
+        left:e.pageX+20,
+        top:e.pageY+20
+    });
+}).on('mouseout',function () {
+    $('#tip').remove();
+    $(this).attr('title',this.currTitle);
+});
+
+
+
+//最新动态

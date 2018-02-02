9598d97885dfc8542f0fd28b0ce33d1a2b1a16f6
tengyue1997 <632013215@qq.com>
2018-02-02 22:19:56 +0800

    购物车demo

---
+++ vuecli/src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="test">
+      <h2>我是测试页面1</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .test{
+    background:#f00;
+    height:300px;
+  }
+</style>
